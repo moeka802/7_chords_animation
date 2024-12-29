6140bf5f89594b397eae0d4b4499c67b3bce639c
moeka802 <moeka802.redapple@gmail.com>
2024-12-29 09:53:29 +0900

    create chord button

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,41 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
 	return (
 		<View
-			style={{
-				flex: 1,
-				justifyContent: "center",
-				alignItems: "center",
-			}}
+			style={[
+				{
+					flex: 1,
+				},
+				styles.center,
+			]}
 		>
-			<Text>Edit app/index.tsx to edit this screen.</Text>
+			<View style={[styles.box, styles.center]}>
+				<Text style={styles.boxText}>C</Text>
+			</View>
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	box: {
+		width: 70,
+		aspectRatio: 1 / 1,
+		borderRadius: 35,
+		backgroundColor: "#fff",
+		shadowColor: "#000",
+		shadowOpacity: 0.2,
+		shadowOffset: { width: 2, height: 2 },
+		shadowRadius: 15,
+		elevation: 3,
+	},
+	boxText: {
+		fontSize: 28,
+		fontWeight: "bold",
+		color: "#6a6a6a",
+	},
+	center: {
+		justifyContent: "center",
+		alignItems: "center",
+	},
+});
